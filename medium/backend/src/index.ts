2ade52a8061318365c918c8a7e3fa58b48c5c9f9
shreyas0924 <shreyasp0924@gmail.com>
2024-02-24 20:15:09 +0530

    signup and signin working

--- medium/backend/src/index.ts
+++ medium/backend/src/index.ts
@@ -33,8 +33,24 @@ app.post("/api/v1/signup", async (c) => {
   }
 });
 
-app.post("/api/v1/signin", (c) => {
-  return c.text("Hello world");
+app.post("/api/v1/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const body = await c.req.json();
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+    },
+  });
+
+  if (!user) {
+    c.status(403);
+    return c.json({ error: "User not found" });
+  }
+
+  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+  return c.json({ jwt });
 });
 app.post("/api/v1/blog", (c) => {
   return c.text("Hello world");
